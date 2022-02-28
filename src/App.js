dfd91d9cbb27b697acfd45ead08abc62e7c7aca8
USERNAME <EMAIL>
2022-02-28 09:29:16 +0000

    Add new blog version

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog version by HH
+            Serin&Doyoon's Happy House
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
