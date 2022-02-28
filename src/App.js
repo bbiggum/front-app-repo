dca33edbd7e54798f10be2718b3d2d8b6fdbe9f7
USERNAME <EMAIL>
2022-02-28 09:09:32 +0000

    Add new blog v by Hyehun'

--- src/App.js
+++ src/App.js
@@ -64,7 +64,7 @@ function App(props) {
             align="center"
             className={classes.title}
           >
-            EKS DEMO Blog
+            EKS DEMO Blog version by Hyehun
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
